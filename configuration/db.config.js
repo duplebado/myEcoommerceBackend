5eb37291c23d3114d865a3469270bd9447e67705
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-10 07:59:17 +0100

    update

--- configuration/db.config.js
+++ configuration/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  Host: "us-cdbr-east-05.cleardb.net",
-  user: "ba7add9cf4c302",
-  password: "12ce3ded",
-  db: "heroku_e854c4c334a1a38",
+  Host: "host",
+  user: "root",
+  password: "jidiamond1reset2",
+  db: "gbogbonise",
 };
