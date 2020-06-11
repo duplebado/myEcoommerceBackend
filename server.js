21646796c1115936237cbe79c2074333f01b3ad1
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-11 20:11:39 +0100

    update

--- server.js
+++ server.js
@@ -7,29 +7,40 @@ const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
-// app.use(
-//     fileUpload({
-//         useTempfiles: true,
-//     }));
-
-// cloudinary.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.API_KEY,
-//     api_secret: process.env.API_SECRET
-// })
+const corsOptions = {
+  origin: [
+    "https://gbogboniseonlinecarshop.netlify.com/",
+    "http://localhost:8080",
+  ],
+  methods: ["POST", "PATCH", "PUT"],
+  // credentials: true,
+  maxAge: 3600,
+};
+app.use(function (req, response, next) {
+  // response.setHeader("Access-Control-Allow-Origin", "https://shopman.netlify.app");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,PATCH"
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hi there, app is running!");
 });
 
 require("./routes/car.routes.js")(app);
-// require("./routes/order.routes.js")(app);
-// require("./routes/shoppingCart.router.js")(app);
 require("./routes/createAccount.routes.js")(app);
 require("./routes/login.routes.js")(app);
 
