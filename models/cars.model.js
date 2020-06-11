6f404dcafb23aa5c348a1fb43d6d93ac30a39ad4
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-11 09:35:05 +0100

    update

--- models/cars.model.js
+++ models/cars.model.js
@@ -19,6 +19,7 @@ const Car = function (car) {
 };
 
 console.log("Hello");
+console.log("Bello");
 
 Car.create = (newCar, result) => {
   sql.query(`INSERT INTO vehicles SET?`, newCar, (err, res) => {
